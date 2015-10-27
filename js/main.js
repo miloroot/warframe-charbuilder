845079bc85549fe9d064b9c952976fd1ab18da4e
Milo <milonice@hotmail.com>
2015-10-27 13:03:59 +0100

    Add: List is now being filled via a JSON file.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 var WarframeModule = ( function( window, undefined ) {
 
   var $theForm = $( '.the-form'),
-      $frameList = $( '.frame-list');
+      $frameList = $( '.frame-list'),
+      $startTag = '<option>',
+      $endTag = '</option>';
 
   function loadFrames() {
     $.ajax({
       url: 'data/warframes.json',
       dataType: 'json',
       success: function( data ) {
-        console.log( "warframes: ", data );
+        for( var i = 0; i < data.length; i++ ) {
+          $frameList.append( $startTag + data[i] + $endTag );
+        }
       },
       error: function( data ) {
-        console.log( "error: ", data );
+        console.log( "error @ function loadFrames: ", data );
       }
     });
   }
